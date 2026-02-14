7b9b7e12434ee07246c94b12397c0a93195ec2df
Daisu2001 <migueescobar01@gmail.com>
2026-02-14 16:17:47 -0500

    Finalización Unidad 2

---
+++ Unidad2/Investigación/Actividad4.asm
@@ -0,0 +1,31 @@
+
+@sum
+M=0      
+@i
+M=1      
+
+(LOOP)
+
+    @i
+    D=M      
+    @100
+    D=D-A    
+    @END
+    D;JGT    
+
+  
+    @i
+    D=M     
+    M=D+M    
+
+ 
+    @i
+    M=M+1    
+
+  
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP    
